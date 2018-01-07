6c1352d33cdbba2f8bfdff2dc37a67b03868b188
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2018-01-07 07:53:07 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+
+    var element = document.createElement('table'); // create table element
+
+    for (var i = 0; i <= 10; i++) {
+        
+        var tr = document.createTextNode('tr')  // create text node tr 1,2,3...
+
+        for (var j = 0; j <= 10; j++) {
+
+            var td = document.createTextNode('td'); // create text node td  1,2,3...
+            td.element.appendChild(tr);
+
+
+        }
+    }
+
+
+
+
+
+};
