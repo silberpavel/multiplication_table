25e94d2e2f093245c0f6d768f85de4d16615f287
Silber <silber.ypm@gmail.com>
2018-01-08 14:18:05 +0500

    init_

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # multiplication_table
+
+* Taken from JS2 (2/1) Bauman university by Almazov
+
+<a href="https://ibb.co/eia596"><img src="https://preview.ibb.co/fFBdU6/temp.png" alt="temp" border="0"></a>
--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 window.onload = function () {
 
-    var element = document.createElement('table'); // create table element
 
-    for (var i = 0; i <= 10; i++) {
-        
-        var tr = document.createTextNode('tr')  // create text node tr 1,2,3...
+function MultiTable(x,y) {
 
-        for (var j = 0; j <= 10; j++) {
+    this.init_ = function() {
 
-            var td = document.createTextNode('td'); // create text node td  1,2,3...
-            td.element.appendChild(tr);
-
-
+        this.table = document.createElement('table'); // create table element
+    
+        for (var i = 1; i <= x; i++) {
+            
+            var tr = document.createElement('tr')  // create text node tr 1,2,3...
+    
+            for (var j = 1; j <= y; j++) {
+    
+                var td = document.createTextNode(`${i} x ${j} = ${i * j}\t`); // create text node td  1,2,3...
+                tr.appendChild(td);   // appent td inside tr
+            }
+    
+            this.table.appendChild(tr);
         }
     }
+
+    this.show = function() {
+        document.body.appendChild(this.table);
+    }
+
+    this.init_();
+
+}
+
+
+// MultiTable(50,50);
+// MultiTable(2,5);
+// MultiTable(5,5);
+
+//  var tab1 = new MultiTable(5,5);
+
+(new MultiTable(5,5)).show();
+
+
+
+
 
 
 
